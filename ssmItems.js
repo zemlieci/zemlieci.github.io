7d52d42002c46e67991b2b4bf34ef66cb5eed31d
zemlieci <alderamin@protonmail.com>
2024-08-16 12:57:20 +0200

    Add files via upload

---
+++ ssmItems.js
@@ -0,0 +1,59 @@
+<!--
+
+/*
+Configure menu styles below
+NOTE: To edit the link colors, go to the STYLE tags and edit the ssm2Items colors
+*/
+YOffset=80; // no quotes!!
+XOffset=0;
+staticYOffset=30; // no quotes!!
+slideSpeed=20 // no quotes!!
+waitTime=100; // no quotes!! this sets the time the menu stays out for after the mouse goes off it.
+menuBGColor="#07505c";
+menuIsStatic="yes"; //this sets whether menu should stay static on the screen
+menuWidth=150; // Must be a multiple of 10! no quotes!!
+menuCols=1;
+hdrFontFamily="Arial";
+hdrFontSize="2";
+hdrFontColor="#ffff00";
+hdrBGColor="#064a4a";
+hdrAlign="left";
+hdrVAlign="center";
+hdrHeight="25";
+linkFontFamily="Arial";
+linkFontSize="2";
+linkBGColor="#1b6773";
+linkOverBGColor="#064a4a";
+linkTarget="_top";
+linkHeight="18";
+linkAlign="Left";
+barBGColor="#0000cc";
+barFontFamily="Verdana";
+barFontSize="2";
+barFontColor="#ffffff";
+barVAlign="center";
+barWidth=20; // no quotes!!
+barText="Navigacia"; //<IMG>tag supported. Put exact html for an image to show.
+
+///////////////////////////
+
+// ssmItems[...]=[name, link, target, colspan, endrow?] - leave 'link' and 'target' blank to make a header
+//ssmItems[0]=["Menu"] //create header
+//ssmItems[1]=["zber.html", ""]
+//ssmItems[2]=["zber.html", "<A href="zber.html" target="jadro">Zbiera",""]
+//ssmItems[3]=["What's Hot", "http://www.dynamicdrive.com/hot.htm", ""]
+//ssmItems[4]=["Message Forum", "http://www.codingforums.com", "_new"]
+//ssmItems[5]=["Submit Script", "http://www.dynamicdrive.com/submitscript.htm", ""]
+//ssmItems[6]=["Link to Us", "http://www.dynamicdrive.com/link.htm", ""]
+
+//ssmItems[7]=["FAQ", "http://www.dynamicdrive.com/faqs.htm", "", 1, "no"] //create two column row
+//ssmItems[8]=["Email", "http://www.dynamicdrive.com/contact.htm", "",1]
+
+//ssmItems[9]=["External Links", "", ""] //create header
+//ssmItems[10]=["JavaScript Kit", "http://www.javascriptkit.com", ""]
+//ssmItems[11]=["Freewarejava", "http://www.freewarejava.com", ""]
+//ssmItems[12]=["Coding Forums", "http://www.codingforums.com", ""]
+
+
+
+//-->
